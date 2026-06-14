e6d7bac248616923ec745fba2df5d3a7b940b324
rohitdhaara-dotcom <rohitdhaara@gmail.com>
2026-06-14 14:56:59 +0530

    Update excalidrawWrapper.tsx 3

--- examples/with-nextjs/src/excalidrawWrapper.tsx
+++ examples/with-nextjs/src/excalidrawWrapper.tsx
@@ -8,3 +8,19 @@ export default function ExcalidrawWrapper() {
     </div>
   );
 }
+"use client";
+import dynamic from "next/dynamic";
+
+// Dynamically import Excalidraw and force Next.js to skip Server-Side Rendering
+const Excalidraw = dynamic(
+  () => import("@excalidraw/excalidraw").then((mod) => mod.Excalidraw),
+  { ssr: false }
+);
+
+export default function ExcalidrawWrapper() {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Excalidraw />
+    </div>
+  );
+}
