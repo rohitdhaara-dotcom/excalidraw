efd8571432cde11dbdca0a9b4784f38f8f80229d
rohitdhaara-dotcom <rohitdhaara@gmail.com>
2026-06-14 14:45:51 +0530

    Update excalidrawWrapper.tsx2

--- examples/with-nextjs/src/excalidrawWrapper.tsx
+++ examples/with-nextjs/src/excalidrawWrapper.tsx
@@ -4,11 +4,7 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 export default function ExcalidrawWrapper() {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      <Excalidraw 
-        initialData={{
-          appState: { viewBackgroundColor: "#ffffff" },
-        }} 
-      />
+      <Excalidraw />
     </div>
   );
 }
